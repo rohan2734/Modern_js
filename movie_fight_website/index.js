93bfe747d2c2f4aa88c41bd8737abadff6255890
rohan2734 <jagandevaki1@gmail.com>
2020-11-11 23:07:29 +0530

    added images, two options for connecting index.js and index.html,the best option, and auto complete widget

--- movie_fight_website/index.js
+++ movie_fight_website/index.js
@@ -15,7 +15,22 @@ const fetchData = async searchTerm => {
   return response.data.Search;
 } 	
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML=`
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown ">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results">
+      </div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown"); //to open the dropdown
+const resultsWrapper = document.querySelector(".results"); //to add the content
+
 
 //debounce shifted to utils.js
 
@@ -23,18 +38,34 @@ const input = document.querySelector("input");
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
   
-  for (let movie of movies) {
-    const div  = document.createElement("div");
+  dropdown.classList.add("is-active");
 
-    div.innerHTML = `
-      <img src= "${movie.Poster}" />
-      <h1>${movie.Title}</h1>
+  resultsWrapper.innerHTML='';
+
+
+
+  for (let movie of movies) {
+    const option  = document.createElement("a");
+    const imgSRC = movie.Poster === "N/A" ? "": movie.Poster;
+
+    option.classList.add("dropdown-item")
+    option.innerHTML = `
+      <img src= "${imgSRC}" />
+      ${movie.Title}
     `;
 
-    document.querySelector("#target").appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
 
 input.addEventListener("input",debounce(onInput,500));
 
+document.addEventListener("click",event => {
+  // console.log(event.target);
+
+  //if root element doesnt contain the element we clicked on
+  if(!root.contains(event.target)){
+    dropdown.classList.remove("is-active");
+  }
+})
