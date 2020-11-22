475a95582dd2d08e16ab89ad227ffbe04495b1da
rohan2734 <jagandevaki1@gmail.com>
2020-11-22 11:43:50 -0800

    added process.cwd

--- js_with_nodejs/list_files_project/index.js
+++ js_with_nodejs/list_files_project/index.js
@@ -1,2 +1,23 @@
 const fs  = require("fs");
 
+fs.readdir(process.cwd(),(err,filenames) => {
+  //Either
+  //err === an object ,which means something went wrong
+  //OR
+  // err === null, which means everything is ok
+  
+  if(err){
+    //error handling code
+    console.log(err);
+    //throw new Error(err);
+    return;
+  }
+
+  console.log(filenames);
+});
+
+
+
+
+
+
