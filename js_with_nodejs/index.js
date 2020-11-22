2de75ecfe759af32c5f1cf85992194a0d1ba9bfd
rohan2734 <jagandevaki1@gmail.com>
2020-11-22 11:24:43 -0800

    debugging in node

--- js_with_nodejs/index.js
+++ js_with_nodejs/index.js
@@ -1,2 +1,16 @@
-const message  = require("./myscript");
-console.log(message);
+// const message  = require("./myscript");
+// require("./myscript");
+
+// require("./myscript");
+
+// console.log(message);
+// console.log(require.cache);
+const counterObject = require("./myscript");
+
+console.log(counterObject.getCounter());//0
+counterObject.incrementCounter() //counter+=1
+console.log(counterObject.getCounter()); //1
+
+const newCounterObject = require("./myscript"); 
+
+console.log(newCounterObject.getCounter()); //0 || 1
--- js_with_nodejs/myscript.js
+++ js_with_nodejs/myscript.js
@@ -1,3 +1,13 @@
-const message = "hi there";
+// const message = "hi there";
 
-module.exports = message;
+// module.exports = message;
+let counter = 0;
+
+module.exports = {
+    incrementCounter(){
+        counter+=1;
+    },
+    getCounter(){
+        return counter;
+    }
+};
