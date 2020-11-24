4f15436086a090648cc8daf8b1bb96df933ee8e3
rohan2734 <jagandevaki1@gmail.com>
2020-11-24 10:29:27 -0800

    created express webserver

--- e-commerce-project/index.js
+++ e-commerce-project/index.js
@@ -1,1 +1,14 @@
-console.log("hi there");
+const express = require("express");
+
+const app = express(); 
+
+app.get("/",(req,res) => { 
+    res.send("hi there")
+}); //if anyone sends a request to "/", then we want run the callback function
+// and send the response.
+
+app.listen(3000,() => {
+    console.log("listening");
+});
+
+
